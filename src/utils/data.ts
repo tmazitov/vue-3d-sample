853c0b75bd044383f1a3e7a3531bc03f7f5b4fe6
tmazitov <timurmazitov000@gmail.com>
2026-03-03 08:56:42 +0400

    update models names

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,9 +1,9 @@
 import { Model } from "./model";
 
 const models:Array<Model> = [
-    new Model(1, 'Phoenix Car', 'models/phoenix/scene.gltf', "Standard size and resolution model that loads perfectly on different devices."),
-    new Model(2, 'Dragon Statue', 'models/dragon/scene.gltf', "Small-scale model with high detail."),
-    new Model(3, 'Nissan Nismo', 'models/nissan/scene.gltf', "Large model that requires significant resources. Ideal for stress-testing smartphones and computers (without GPUs)."),
+    new Model(1, 'Phoenix Car (11,8 mb)', 'models/phoenix/scene.gltf', "Standard size and resolution model that loads perfectly on different devices."),
+    new Model(2, 'Dragon Statue (14,4 mb)', 'models/dragon/scene.gltf', "Small-scale model with high detail."),
+    new Model(3, 'Nissan Nismo (113,9 mb)', 'models/nissan/scene.gltf', "Large model that requires significant resources. Ideal for stress-testing smartphones and computers (without GPUs)."),
 ]
 
 if (models.length == 0) {
